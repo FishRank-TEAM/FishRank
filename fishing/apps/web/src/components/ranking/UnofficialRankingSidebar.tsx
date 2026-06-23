9bc6e64c79539c968bfbb17e31e3d672aa15ce06
rjsgud49 <qkrrjsgud49@gmail.com>
2026-06-23 10:13:02 +0900

    refactor(web): improve unofficial ranking sidebar

--- fishing/apps/web/src/components/ranking/UnofficialRankingSidebar.tsx
+++ fishing/apps/web/src/components/ranking/UnofficialRankingSidebar.tsx
@@ -1,10 +1,17 @@
 'use client';
 
 import BragEvaluateFeed from './BragEvaluateFeed';
+import BragTopUsersMarquee from './BragTopUsersMarquee';
+import type { RankingItem } from '@/components/RankingCard';
 
-export default function UnofficialRankingSidebar() {
+type Props = {
+  topItems: RankingItem[];
+};
+
+export default function UnofficialRankingSidebar({ topItems }: Props) {
   return (
     <aside className="brag-sidebar">
+      <BragTopUsersMarquee items={topItems} />
       <BragEvaluateFeed />
     </aside>
   );
