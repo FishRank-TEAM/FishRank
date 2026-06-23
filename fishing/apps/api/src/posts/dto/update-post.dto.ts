4ef19ec84ac6d55f8b5c0d29f1423e0cc27eb942
rjsgud49 <qkrrjsgud49@gmail.com>
2026-06-23 09:03:40 +0900

    feat(api): add posts dto update-post.dto

---
+++ fishing/apps/api/src/posts/dto/update-post.dto.ts
@@ -0,0 +1,18 @@
+import { IsOptional, IsString, MaxLength } from 'class-validator';
+
+export class UpdatePostDto {
+  @IsString()
+  @MaxLength(100)
+  title: string;
+
+  @IsString()
+  content: string;
+
+  @IsOptional()
+  @IsString()
+  catchId?: string;
+
+  @IsOptional()
+  @IsString()
+  removeImage?: string;
+}
