2e74c50e98e8eeb93a67cdf85df83e8ac1f6a44d
rjsgud49 <qkrrjsgud49@gmail.com>
2026-06-23 09:05:41 +0900

    feat(web): add UnofficialRankingSidebar component

---
+++ fishing/apps/web/src/components/ranking/UnofficialRankingSidebar.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import BragEvaluateFeed from './BragEvaluateFeed';
+
+export default function UnofficialRankingSidebar() {
+  return (
+    <aside className="brag-sidebar">
+      <BragEvaluateFeed />
+    </aside>
+  );
+}
