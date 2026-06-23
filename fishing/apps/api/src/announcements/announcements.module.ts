12ea71a2d0266c196b5482de10179730cca5c5a6
rjsgud49 <qkrrjsgud49@gmail.com>
2026-06-23 09:03:29 +0900

    feat(api): add announcements announcements.module

---
+++ fishing/apps/api/src/announcements/announcements.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { AnnouncementsController } from './announcements.controller';
+import { AdminModule } from '../admin/admin.module';
+
+@Module({
+  imports: [AdminModule],
+  controllers: [AnnouncementsController],
+})
+export class AnnouncementsModule {}
